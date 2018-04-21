74dfe90b67e1bdc64772697591c70661c0ddbc99
Florian Posdziech <hallo@flowfx.de>
2018-04-20 19:04:05 -0500

    Revert "Fix fog configuration"
    This reverts commit cb9fab02bf826f89a731552a8b6a993425d5afca.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,9 +5,9 @@ if Rails.env.production?
       :provider              => 'backblaze',
       :b2_account_id         => ENV['B2_ACCOUNT_ID'],
       :b2_account_token      => ENV['B2_ACCOUNT_TOKEN'],
-      :b2_bucket_name        => ENV['B2_BUCKET_NAME'],
-      :b2_bucket_id          => ENV['B2_BUCKET_ID'],
+    #   :b2_bucket_name        => ENV['B2_BUCKET_NAME'],
+    #   :b2_bucket_id          => ENV['B2_BUCKET_ID'],
     }
-    # config.fog_directory     =  ENV['B2_BUCKET_ID']
+    config.fog_directory     =  ENV['B2_BUCKET_ID']
   end
 end
