7e8e04956ec88a2382ff6dc1cc987e52d3c31f03
Florian Posdziech <hallo@flowfx.de>
2018-04-20 19:06:47 -0500

    Config fog with B2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,16 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/backblaze'
+
     config.fog_credentials = {
-      # Configuration for Amazon S3
+      # Configuration for Backblaze B2
       :provider              => 'backblaze',
       :b2_account_id         => ENV['B2_ACCOUNT_ID'],
       :b2_account_token      => ENV['B2_ACCOUNT_TOKEN'],
-    #   :b2_bucket_name        => ENV['B2_BUCKET_NAME'],
-    #   :b2_bucket_id          => ENV['B2_BUCKET_ID'],
+
+      :b2_bucket_name        => ENV['B2_BUCKET_NAME'],
+      :b2_bucket_id          => ENV['B2_BUCKET_ID'],
     }
-    config.fog_directory     =  ENV['B2_BUCKET_ID']
+    config.fog_directory     =  ENV['B2_BUCKET_NAME']
   end
 end
