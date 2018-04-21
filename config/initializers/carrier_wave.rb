52ac91c60ee819e5f6bab4ca27b5bc2f79c9ddc9
Florian Posdziech <hallo@flowfx.de>
2018-04-20 19:10:09 -0500

    fix fog config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,13 +4,15 @@ if Rails.env.production?
 
     config.fog_credentials = {
       # Configuration for Backblaze B2
-      :provider              => 'backblaze',
-      :b2_account_id         => ENV['B2_ACCOUNT_ID'],
-      :b2_account_token      => ENV['B2_ACCOUNT_TOKEN'],
-
-      :b2_bucket_name        => ENV['B2_BUCKET_NAME'],
-      :b2_bucket_id          => ENV['B2_BUCKET_ID'],
+      provider: 'backblaze',
+      b2_account_id: ENV['B2_ACCOUNT_ID'],
+      b2_account_token: ENV['B2_ACCOUNT_TOKEN'],
+      logger: Rails.logger,
+      b2_bucket_name: ENV['B2_BUCKET_NAME'],
+      b2_bucket_id: ENV['B2_BUCKET_ID'],
     }
-    config.fog_directory     =  ENV['B2_BUCKET_NAME']
+    config.fog_directory = ENV['B2_BUCKET_NAME']
+
+    config.fog_public = true
   end
 end
